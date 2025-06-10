e01ad4e76d40b57c9bd9fc88b95e2d7dd8fae114
root <root@kulaiyin.localdomain>
2025-06-10 23:36:40 +0800

    [fix]: 更新vitepress.conf

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-yarn i
+yarn
 yarn docs:build
 cp vitepress.conf /etc/nginx/conf.d
+rm -rf /var/www/html/vitepress
+cp -r markdown/.vitepress/dist /var/www/html/vitepress
 systemctl reload nginx.service
