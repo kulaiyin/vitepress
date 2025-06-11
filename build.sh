284a43357177d943111a5a1884a7520834567eba
kulaiyin <kly0214@163.com>
2025-06-11 22:59:17 +0800

    [fix]: 更新build.sh脚本

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ cp vitepress.conf /etc/nginx/conf.d
 rm -rf /var/www/html/vitepress
 cp -r markdown/.vitepress/dist /var/www/html/vitepress
 systemctl reload nginx.service
-
-http://www.klydev.com:3056
